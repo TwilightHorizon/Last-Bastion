6fbe8e9c357305ad3e92d9671f7fea55e11becd4
Sean (Seung Hoon) Lee <seanisgood2006@gmail.com>
2025-02-19 16:05:49 -0500

    Made db update and get for upgrades

--- Assets/Scripts/BackendGameUpgradeData.cs
+++ Assets/Scripts/BackendGameUpgradeData.cs
@@ -82,7 +82,35 @@ public class BackendGameUpgradeData
                     {
                         gameUpgradeDataRowInDate = gameDataJson[0]["inDate"].ToString();
 
+                        foreach (string itemKey in gameDataJson[0]["towerOneUpgrade"].Keys)
+                        {
+                            BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade.Add(itemKey, int.Parse(gameDataJson[0]["towerOneUpgrade"][itemKey].ToString()));
+                        }
 
+                        foreach (string itemKey in gameDataJson[0]["towerTwoUpgrade"].Keys)
+                        {
+                            BackendGameUpgradeData.Instance.UserGameUpgradeData.towerTwoUpgrade.Add(itemKey, int.Parse(gameDataJson[0]["towerTwoUpgrade"][itemKey].ToString()));
+                        }
+
+                        foreach (string itemKey in gameDataJson[0]["towerThreeUpgrade"].Keys)
+                        {
+                            BackendGameUpgradeData.Instance.UserGameUpgradeData.towerThreeUpgrade.Add(itemKey, int.Parse(gameDataJson[0]["towerThreeUpgrade"][itemKey].ToString()));
+                        }
+
+                        foreach (string itemKey in gameDataJson[0]["towerFourUpgrade"].Keys)
+                        {
+                            BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFourUpgrade.Add(itemKey, int.Parse(gameDataJson[0]["towerFourUpgrade"][itemKey].ToString()));
+                        }
+
+                        foreach (string itemKey in gameDataJson[0]["towerFiveUpgrade"].Keys)
+                        {
+                            BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFiveUpgrade.Add(itemKey, int.Parse(gameDataJson[0]["towerFiveUpgrade"][itemKey].ToString()));
+                        }
+
+                        foreach (string itemKey in gameDataJson[0]["towerSixUpgrade"].Keys)
+                        {
+                            BackendGameUpgradeData.Instance.UserGameUpgradeData.towerSixUpgrade.Add(itemKey, int.Parse(gameDataJson[0]["towerSixUpgrade"][itemKey].ToString()));
+                        }
 
                         onGameDataLoadEvent?.Invoke();
 
@@ -103,5 +131,47 @@ public class BackendGameUpgradeData
             }
         });
     }
+    public void GameDataUpdate(UnityAction action = null)
+    {
+        if (userGameUpgradeData == null)
+        {
+            Debug.LogError("서버에서 다운받거나 새로 삽입한 데이터가 존재하지 않습니다.");
+            return;
+        }
 
+        Param param = new Param()
+        {
+            { "towerOneUpgrade", userGameUpgradeData.towerOneUpgrade },
+            { "towerTwoUpgrade", userGameUpgradeData.towerTwoUpgrade },
+            { "towerThreeUpgrade", userGameUpgradeData.towerThreeUpgrade },
+            { "towerFourUpgrade", userGameUpgradeData.towerFourUpgrade },
+            { "towerFiveUpgrade", userGameUpgradeData.towerFiveUpgrade },
+            { "towerSixUpgrade", userGameUpgradeData.towerSixUpgrade }
+        };
+
+        if (string.IsNullOrEmpty(gameUpgradeDataRowInDate))
+        {
+            Debug.LogError("유저의 inDate 정보가 없어서 게임 데이터 수정에 실패");
+
+        }
+
+        else
+        {
+            Debug.Log($"{gameUpgradeDataRowInDate}의 게임 데이터 정보 수정을 요청합니다.");
+
+            Backend.GameData.UpdateV2("USER_UPGRADE", gameUpgradeDataRowInDate, Backend.UserInDate, param, callback =>
+            {
+                if (callback.IsSuccess())
+                {
+                    Debug.Log($"게임 정보 수정 성공, {callback}");
+                    action?.Invoke();
+                }
+                else
+                {
+                    Debug.LogError($"실패 수정 정보 : {callback}");
+                }
+            });
+        }
+
+    }
 }
--- Assets/Scripts/MarketControl.cs
+++ Assets/Scripts/MarketControl.cs
@@ -1,117 +1,148 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MarketControl : MonoBehaviour
 {
 
-    
+    [SerializeField]
+    private TextMeshProUGUI[] buttonTexts;
+
+
+
+
+    private void Awake()
+    {
+        StartCoroutine(UpdateUpgrade());
+    }
+
+    private IEnumerator UpdateUpgrade()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(300);
+            BackendGameUpgradeData.Instance.GameDataUpdate();
+            BackendGameData.Instance.GameDataUpdate();
+        }
+    }
+
+
+
+
+
+
+
     public void ButtonOne()
     {
         // tower 1 damage
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade["damage"] += 1;
+        buttonTexts[0].text = "Damage Lv." + BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade["damage"].ToString() 
+            + $"{20* BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade["damage"]}";
+        BackendGameData.Instance.UserGameData.gold -= 20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade["damage"];
     }
     public void ButtonTwo()
     {
-        // tower 1 speed
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade["attackSpeed"] += 1;
+        buttonTexts[1].text = $"Attack Speed Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade["attackSpeed"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade["attackSpeed"]})";
     }
 
     public void ButtonThree()
     {
-        // tower 1 range
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade["range"] += 1;
+        buttonTexts[2].text = $"Attack Range Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade["range"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade["range"]})";
     }
 
     public void ButtonFour()
     {
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade["cost"] += 1;
+        buttonTexts[3].text = $"Price Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade["cost"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerOneUpgrade["cost"]})";
     }
 
 
     public void ButtonFive()
     {
-        // tower 2 damage
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerTwoUpgrade["damage"] += 1;
+        buttonTexts[4].text = $"Damage Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerTwoUpgrade["damage"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerTwoUpgrade["damage"]})";
     }
 
     public void ButtonSix()
     {
-        // tower 2 range
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerTwoUpgrade["range"] += 1;
+        buttonTexts[5].text = $"Attack Range Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerTwoUpgrade["range"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerTwoUpgrade["range"]})";
     }
 
     public void ButtonSeven()
     {
-        // tower 2 cost
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerTwoUpgrade["cost"] += 1;
+        buttonTexts[6].text = $"Price Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerTwoUpgrade["cost"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerTwoUpgrade["cost"]})";
     }
 
     public void ButtonEight()
     {
-        // tower 3 slow
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerThreeUpgrade["slow"] += 1;
+        buttonTexts[7].text = $"Slow Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerThreeUpgrade["slow"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerThreeUpgrade["slow"]})";
     }
 
     public void ButtonNine()
     {
-        // tower 3 range
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerThreeUpgrade["range"] += 1;
+        buttonTexts[8].text = $"Attack Range Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerThreeUpgrade["range"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerThreeUpgrade["range"]})";
     }
 
     public void ButtonTen()
     {
-        // tower 3 cost
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerThreeUpgrade["cost"] += 1;
+        buttonTexts[9].text = $"Price Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerThreeUpgrade["cost"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerThreeUpgrade["cost"]})";
     }
 
     public void ButtonEleven()
     {
-        // tower 4 buff
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFourUpgrade["buff"] += 1;
+        buttonTexts[10].text = $"Buff Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFourUpgrade["buff"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFourUpgrade["buff"]})";
     }
 
     public void ButtonTwelve()
     {
-        // tower 4 range
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFourUpgrade["range"] += 1;
+        buttonTexts[11].text = $"Attack Range Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFourUpgrade["range"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFourUpgrade["range"]})";
     }
 
     public void ButtonThirteen()
     {
-        // tower 4 cost
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFourUpgrade["cost"] += 1;
+        buttonTexts[12].text = $"Price Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFourUpgrade["cost"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFourUpgrade["cost"]})";
     }
 
     public void ButtonFourteen()
     {
-        // tower 5 earning
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFiveUpgrade["earning"] += 1;
+        buttonTexts[13].text = $"Earning Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFiveUpgrade["earning"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFiveUpgrade["earning"]})";
     }
 
     public void ButtonFifteen()
     {
-        // tower 5 cost
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFiveUpgrade["cost"] += 1;
+        buttonTexts[14].text = $"Price Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFiveUpgrade["cost"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerFiveUpgrade["cost"]})";
     }
 
     public void ButtonSixteen()
     {
-        // tower 6 damage
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerSixUpgrade["damage"] += 1;
+        buttonTexts[15].text = $"Damage Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerSixUpgrade["damage"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerSixUpgrade["damage"]})";
     }
 
     public void ButtonSeventeen()
     {
-        // tower 6 range
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerSixUpgrade["range"] += 1;
+        buttonTexts[16].text = $"Attack Range Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerSixUpgrade["range"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerSixUpgrade["range"]})";
     }
 
     public void ButtonEighteen()
     {
-        // tower 6 cost
         BackendGameUpgradeData.Instance.UserGameUpgradeData.towerSixUpgrade["cost"] += 1;
+        buttonTexts[17].text = $"Price Lv.{BackendGameUpgradeData.Instance.UserGameUpgradeData.towerSixUpgrade["cost"]} ({20 * BackendGameUpgradeData.Instance.UserGameUpgradeData.towerSixUpgrade["cost"]})";
     }
 
 
